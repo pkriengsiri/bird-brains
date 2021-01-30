864f13f6c462d9f1aac41320d87bf7e5194b4529
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-29 20:56:39 -0500

    Added API PUT route to edit user in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,3 +5,19 @@ const db = require("../models");
 // Creates a new router object
 const router = express.router();
 
+// API route to edit a single user
+router.put("/users/:id", (req, res) => {
+  db.User.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((user) => {
+      console.log(user);
+      res.json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
