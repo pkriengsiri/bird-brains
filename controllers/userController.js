aeccfbd5e353bba6214792c11b3aed141988276c
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-29 21:01:34 -0500

    Added API POST route to create a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,8 +5,22 @@ const db = require("../models");
 // Creates a new router object
 const router = express.router();
 
+
+
+// API POST route to create a user
+router.post("/api/users", (req, res) => {
+  db.User.create(req.body)
+    .then((createdUser) => {
+      res.json(createdUser);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
+});
+
 // API route to edit a single user
-router.put("/users/:id", (req, res) => {
+router.put("/api/users/:id", (req, res) => {
   db.User.update(req.body, {
     where: {
       id: req.params.id,
