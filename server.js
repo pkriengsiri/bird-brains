82a4ccb45c403778a3b716f718efe66e4b2be002
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-30 16:52:41 -0500

    Uninstalled unecessary packages

--- server.js
+++ server.js
@@ -6,14 +6,7 @@ const {
   allowInsecurePrototypeAccess,
 } = require("@handlebars/allow-prototype-access");
 const app = express();
-const dotenv = require("dotenv");
-dotenv.config();
-const cloudinary = require("cloudinary").v2;
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+
 
 // require all models
 const db = require("./models");
