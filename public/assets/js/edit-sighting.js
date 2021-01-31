324a1ffee797d0fa4f9658a93307683da77e69ea
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-01-31 10:22:23 -0500

    Updated edit form to pull in information from sequelize query

---
+++ public/assets/js/edit-sighting.js
@@ -0,0 +1,78 @@
+$(document).ready(() => {
+  $("select").formSelect();
+  let imageURL = "";
+  // var instance = M.FormSelect.getInstance(elem);
+
+  var myWidget = cloudinary.createUploadWidget(
+    {
+      cloudName: "djou7v3ho",
+      uploadPreset: "krrvtotm",
+    },
+    function (error, result) {
+      console.log(result.event);
+      if (!error && result && result.event === "success") {
+        $("#successMessage").removeClass("hide");
+        console.log("Done! Here is the image info: ", result.info);
+        imageURL = result.info.url;
+        $("#uploaded-image").empty();
+        const image = $("<img>")
+          .attr("src", imageURL)
+          .addClass("responsive-img z-depth-3");
+        $("#uploaded-image").append(image);
+        console.log(imageURL);
+      } else if (!result.event) {
+        $("#failMessage").removeClass("hide");
+        console.log(error);
+      }
+    }
+  );
+
+  //   $("#submit").on("click", function (event) {
+  //     event.preventDefault();
+  //     const UserId = $("#user_select").val();
+  //     const BirdId = $("#bird_select").val();
+  //     const location = $("#location").val();
+  //     const comments = $("#comments").val();
+  //     const data = {
+  //       location: location,
+  //       comments: comments,
+  //       image_URL: imageURL,
+  //       BirdId: BirdId,
+  //       UserId: UserId,
+  //     };
+
+  //     $.post("/api/sightings", data)
+  //       .then((response) => {
+  //         console.log(response);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+
+  //     const score = $("#user_select :selected").data("score");
+  //     const points = $("#bird_select :selected").data("points");
+  //     const newScore = points + score;
+  //     console.log(newScore);
+
+  //     $.ajax({
+  //       method: "PUT",
+  //       url: "/api/users/" + UserId,
+  //       data: { score: newScore },
+  //     })
+  //       .then((response) => {
+  //         console.log(response);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error);
+  //       });
+  //   });
+
+  document.getElementById("upload_widget").addEventListener(
+    "click",
+    function (event) {
+      event.preventDefault();
+      myWidget.open();
+    },
+    false
+  );
+});
