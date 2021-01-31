2609731074cf04a0b6207b2f710901521f9f5abe
T Schway <tony.schwebach@gmail.com>
2021-01-31 17:55:31 -0500

    added edit and delete buttons for user sightings

---
+++ public/assets/js/user-sightings.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+  $("select").formSelect();
+
+  $(".delete-sighting").on("click", function (event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/sightings/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted sighting", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+});
