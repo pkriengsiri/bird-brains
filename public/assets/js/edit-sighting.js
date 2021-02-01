6cadc5fc3c9a530cc5c13a1d1a3eff80792ac0d3
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-01 13:39:27 -0500

    Fixed bug with image preview in edit.   Added redirect

--- public/assets/js/edit-sighting.js
+++ public/assets/js/edit-sighting.js
@@ -80,6 +80,7 @@ $(document).ready(() => {
     })
       .then((response) => {
         console.log(response);
+        window.location.replace(`/users/${data.UserId}`);
       })
       .catch((error) => {
         console.log(error);
