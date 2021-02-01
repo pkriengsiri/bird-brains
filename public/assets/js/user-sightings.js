c15c223ed463fca5a652febd1a62db6f2ba8bee5
T Schway <tony.schwebach@gmail.com>
2021-02-01 13:07:46 -0500

    delete updates user scores

--- public/assets/js/user-sightings.js
+++ public/assets/js/user-sightings.js
@@ -1,14 +1,22 @@
 $(document).ready(() => {
   $("select").formSelect();
 
+  const score = $("#user_select :selected").data("score");
+  const points = $("#bird_select :selected").data("points");
+  const newScore = score - points;
+  console.log(newScore);
+
   $(".delete-sighting").on("click", function (event) {
+    event.preventDefault()
+    console.log(this)
     var id = $(this).data("id");
 
     // Send the DELETE request.
     $.ajax("/api/sightings/" + id, {
       type: "DELETE",
     })
-      .then(function () {
+      .then(function (res) {
+        console.log(res);
         console.log("deleted sighting", id);
         // Reload the page to get the updated list
         location.reload();
@@ -16,5 +24,7 @@ $(document).ready(() => {
       .catch((error) => {
         console.log(error);
       });
+
+
   });
 });
