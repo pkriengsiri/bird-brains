0aea5cd63cf5eaa611af98933ab28b7fe27eb436
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-02-02 12:46:39 -0500

    Added validation to user model

--- models/sighting.js
+++ models/sighting.js
@@ -1,7 +1,7 @@
 // Creating and exporting the Sightings model
 module.exports = function (sequelize, DataTypes) {
   const Sighting = sequelize.define("Sighting", {
-    location: { type: DataTypes.STRING, allowNull: false },
+    location: { type: DataTypes.STRING, allowNull: false, len: [1, 100] },
     comments: { type: DataTypes.STRING, allowNull: false },
     image_URL: { type: DataTypes.STRING, allowNull: true },
   });
